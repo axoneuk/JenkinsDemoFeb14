037389be2bcf75f72a331549fea552a4d7be9d40
axoneuk <info@axone-tech.uk>
2021-02-14 09:08:23 +0000

    updating application code

--- src/main/java/uk/axone/devintest/maven/Application.java
+++ src/main/java/uk/axone/devintest/maven/Application.java
@@ -4,7 +4,7 @@ package uk.axone.devintest.maven;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Maven is cool");
+        System.out.println("Maven is cool, so is Jenkins");
     }
 
 }
