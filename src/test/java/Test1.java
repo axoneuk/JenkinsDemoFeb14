2c51cc8276da1143caa3785006205a342d8c2fa2
axoneuk <info@axone-tech.uk>
2021-02-14 08:56:21 +0000

    Forcing 2 unit tests to fail

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Test1 {
@@ -6,11 +7,13 @@ public class Test1 {
         @Test
         public void test1(){
             System.out.println("running test 1");
+            Assert.fail();
         }
 
     @Test
     public void test2(){
         System.out.println("running test 2");
+        Assert.fail();
     }
 
     @Test
