8e2dee596161dea54a6819cbb7ecd858b3df883b
axoneuk <info@axone-tech.uk>
2021-02-14 09:02:39 +0000

    Reverting the commit to fail unit tests

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,4 +1,3 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 public class Test1 {
@@ -7,13 +6,11 @@ public class Test1 {
         @Test
         public void test1(){
             System.out.println("running test 1");
-            Assert.fail();
         }
 
     @Test
     public void test2(){
         System.out.println("running test 2");
-        Assert.fail();
     }
 
     @Test
